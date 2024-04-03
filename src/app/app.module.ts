7c4b7ff20cf00e6aa18adda8f8b9ffcf1df75372
Mayhew Seavey <commander.boffo@gmail.com>
2024-04-02 18:39:24 -0700

    Basic login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    provideAuth0({
+      domain: 'mayhew3.auth0.com',
+      clientId: 'HY2lTrNdFc6HDrTlSoKZNL0EriSi0dnW',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
